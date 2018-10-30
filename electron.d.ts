2939b8d6ecdc5ebfce0d3d16e2c8db26c686b00b
Felix <felix@felixrieseberg.com>
2018-10-30 20:51:51 +0000

    Update: Electron to 4.0.0-nightly.20180817

--- electron.d.ts
+++ electron.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Electron 4.0.0-nightly.20180816
+// Type definitions for Electron 4.0.0-nightly.20180817
 // Project: http://electronjs.org/
 // Definitions by: The Electron Team <https://github.com/electron/electron>
 // Definitions: https://github.com/electron/electron-typescript-definitions
